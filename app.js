ff39b7b645e98ec215b04bf3c4b22d75edf4d936
Shikha <sbhatt10@uncc.edu>
2020-04-12 17:14:26 -0400

    Update port

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
+var PORT = process.env.PORT || 80;
 var express = require('express');
 var app = express();
 var index = require('./views/index.js');
 
 app.use('/*', index);
 
-app.listen(80);
+app.listen(PORT);
