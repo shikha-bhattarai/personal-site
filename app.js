47c1423034dbec961949b69c5fe282e61b925a84
Shikha <sbhatt10@uncc.edu>
2020-04-12 20:31:01 -0400

    Add navigation

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
-var PORT = process.env.PORT || 80;
+var PORT = process.env.PORT || 8084;
 var express = require('express');
 var app = express();
 var index = require('./routes/index.js');
 var http = require ('http');
 var server = http.Server(app);
 
-app.use('/*', index);
+app.use('/', index);
+
+app.use('/assets', express.static('assets'));
 
 server.listen(PORT, function(){
     console.log("Application is running");
